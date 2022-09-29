037e4efa1e54f1cae187ebf1428573884d97b25d
lthchinchin <letrunghieu5612@gmail.com>
2022-09-29 16:58:10 +0700

    setup gg map api

--- functions.php
+++ functions.php
@@ -1,3 +1,39 @@
 <?php
 
 require get_template_directory() . '/inc/init.php';
+
+
+
+class Settings_Admin {
+    public function __construct() {
+        add_action( 'wp_enqueue_scripts', array( $this, 'add_custom_style_script' ) );
+        // add_filter( 'script_loader_tag', array( $this, 'defer_js' ), 10, 2 );
+    }
+
+    public function add_custom_style_script() {
+        // wp_enqueue_style( 'wa_email_marketing', get_template_directory_uri() . '/dist/css/main.css' );
+        
+        // wp_enqueue_script( 'wa_email_marketing-js', get_template_directory_uri() . '/dist/js/root.js', array(), false, true );
+        wp_localize_script( 'wa_email_marketing-js', 'vueObject', array(
+            'homeUrl'    => get_home_url(),
+            'currentUri' => $_SERVER['REQUEST_URI'],
+        ));
+
+        wp_enqueue_script('map-render', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAmaslg9P1CTxK8xnDOlOZ1YDJI0Le02XU&libraries=drawing,places,geometry&v=weekly&channel=2', array(), false, false);
+        wp_enqueue_script('markerclustererplus', 'https://unpkg.com/@googlemaps/markerclusterer/dist/index.min.js', array(), false, true);
+
+    }
+
+    // public function defer_js( $tag, $handle ) {
+    //     $scripts_to_defer = array( 'wa_email_marketing' );
+    //     foreach ( $scripts_to_defer as $defer_script ) {
+    //         if ( $defer_script === $handle ) {
+    //             return str_replace( ' src', ' defer src', $tag );
+    //         }
+    //     }
+    //     return $tag;
+    // }
+
+}
+// new Settings_Admin();
+wp_enqueue_script('map-render', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAmaslg9P1CTxK8xnDOlOZ1YDJI0Le02XU&libraries=drawing,places,geometry&v=weekly&channel=2', array(), false, false);
--- src/js/App.vue
+++ src/js/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
+
     <h1>{{ pageTitle }}</h1>
     <p>{{ pageDesc }}</p>
     <button @click="creaseNum">up</button>
     <p>{{ num }}</p>
+      <div style="height:500px" id="map"></div>
+
   </div>
 </template>
 <style scoped>
@@ -18,10 +21,94 @@ export default {
       title: "Hello this trung hieu & vuejs",
       desc: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
       num: 0,
+      map : null
     };
   },
   created: function () {},
+  mounted(){
+      this.initMap();
+  },
   methods: {
+        initMap: function () {
+          console.log('123');
+      let that = this;
+      this.map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: {lat:16.0464896,lng:108.1901056},
+        streetViewControl: false,
+        mapTypeControl: false,
+        fullscreenControl: false,
+        draggable: true,
+        zoomControl: true,
+        scrollwheel: true,
+        disableDoubleClickZoom: true,
+        mapTypeControl: true,
+      });
+
+
+      this.map.setOptions({
+        styles: [
+          {
+            featureType: "poi",
+            elementType: "labels.text",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "poi.business",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "poi.medical",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "road",
+            elementType: "labels.icon",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+          {
+            featureType: "transit",
+            stylers: [
+              {
+                visibility: "off",
+              },
+            ],
+          },
+        ],
+      });
+
+        var marker = new google.maps.Marker({
+          map: this.map,
+          title: "sdsdassd",
+          position: {lat:16.0464896,lng:108.1901056},
+          // icon: iconUrl,
+        });
+
+
+      // if (!this.markers.length) {
+      // this.service = new google.maps.places.PlacesService(this.map);
+
+          console.log('456');
+
+
+      // }
+    },
     testFunc() {
       console.log("123");
     },
